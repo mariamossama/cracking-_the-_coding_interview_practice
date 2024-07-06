c1f1514731828f860bc0fce76f6d82682a5a2fbb
mariamossama <85443737+mariamossama@users.noreply.github.com>
2024-07-06 23:33:17 +0300

    problem 2

--- chapter4/problem2.cpp
+++ chapter4/problem2.cpp
@@ -2,15 +2,11 @@
 #include <bits/stdc++.h>
 #include "../../../../../msys64/mingw64/include/c++/12.2.0/bits/algorithmfwd.h"
 
-int main()
+bool check2Strs(std::string str1 , std::string str2)
 {
-    std::string str1 = "abcd";
-    std::string str2 = "bcda";
-
-
     if(str1.size() != str2.size())
     {
-        std::cout << "no" << std::endl;
+        return false;
     }
 
     sort(str1.begin(), str1.end());
@@ -18,14 +14,47 @@ int main()
 
     for(int i = 0; i < str1.size(); i++)
     {
+
         if(str1[i] != str2[i])
         {
-            std::cout << "no" << std::endl;
+            return false;
         }
 
     }
+    return true;
+}
 
-    std::cout << "yes" << std::endl;
+bool check2strs_v2(std::string str1 , std::string str2)
+{
+    if(str1.size() != str2.size())
+    {
+        return false;
+    }
+
+    bool s1[26] = {0};
+    bool s2[26] = {0};
+
+    for(int i = 0 ; i < str1.size() ; i++)
+    {
+        s1[str1[i]-'a'] = true;
+        s2[str2[i]-'a'] = true;
+    }
+    for(int i = 0 ; i < 26 ; i++)
+    {
+        if(s1[i] != s2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    std::string str1 = "abcd";
+    std::string str2 = "bcda";
+
+    std::cout << check2Strs(str1,str2)<< std::endl;
+    std::cout << check2strs_v2(str1,str2)<< std::endl;
 
 
     return 0 ;
